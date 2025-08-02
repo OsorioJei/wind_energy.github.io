7d36422066e9d93406c72c5ffaa907cbb8b8c6b7
=OsorioJei <osoriojei@gmail.com>
2025-08-02 15:57:33 -0500

    Progress 10%

---
+++ script/script.js
@@ -0,0 +1,11 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+
+  });
+
+  // Or with jQuery
+
+
+ 
+  
